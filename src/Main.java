8d2ebdb75c747b63789156353e23312d702b0cc2
Fred Morrison <frederick.morrison@outlook.com>
2025-03-04 21:44:51 -0700

    Create convertGoogleDrivePublicLinkToUrl method

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,30 @@ import java.net.URI;
 import java.net.URL;
 
 public class Main {
+
+    /** Convert a shared, view-only, public link to a text file stored on Google Drive to one that can be read
+     one line at a time through a combination of BufferedReader and InputStreamReader.
+     * @param link - the original shared, view-only, public link to a text file stored on Google Drive.
+     * @return a URL that will allow a Java BufferedReader to read a text file one line at a time.
+     */
+    private static String convertGoogleDrivePublicLinkToUrl(String link) {
+
+        final String startingToken = "file/d/";
+        final int startingTokenLength = startingToken.length();
+
+        final String endingToken = "/view?usp";
+        final int endingTokenLength = endingToken.length();
+
+        // find start of ID
+        int idxStart = link.indexOf(startingToken) + startingTokenLength;
+
+        // find end of ID
+        int idxEnd = link.indexOf(endingToken);
+
+        // "https://drive.google.com/uc?export=download&id=1Ul-vu8XCKEUkCIrnAGLNOvOvpCN6ypna";
+        return String.format("https://drive.google.com/uc?export=download&id=%s", link.substring(idxStart, idxEnd) );
+    }
+
     public static void main(String[] args) {
         // FAILS: final String fileUrl = "https://1drv.ms/x/s!Ash3pFpgn-Cnyr18zLwmbT6q_S0Psg?e=mk0aeT";
         // FAILS: final String fileUrl = "https://onedrive.live.com/download?id=A7E09F605AA477C8!1220348&resid=A7E09F605AA477C8!1220348&ithint=file%2cxlsx&authkey=!AMy8Jm0-qv0tD7I&wdo=2&cid=a7e09f605aa477c8";
@@ -25,10 +49,15 @@ public class Main {
         // public link from &dl=0 to &dl=1; otherwise, you get HTML instead of text.
         // WORKS: final String fileUrl = "https://www.dropbox.com/s/t8vh8a1rgq41d86/StateData.csv?st=p2b1ypzw&dl=1";
 
-        // https://drive.google.com/file/d/1Ul-vu8XCKEUkCIrnAGLNOvOvpCN6ypna/view?usp=sharing
+        String link = "https://drive.google.com/file/d/1Ul-vu8XCKEUkCIrnAGLNOvOvpCN6ypna/view?usp=sharing";
+        String filename = "args[0]";
+        String fileUrl = convertGoogleDrivePublicLinkToUrl(link);
+        System.out.format("url=%s%n", fileUrl);
+
         // NOTICE how the Google Drive shared public view-only link needs to be modified below.
         // Without this change, you get HTML instead of the actual text:
-        final String fileUrl = "https://drive.google.com/uc?export=download&id=1Ul-vu8XCKEUkCIrnAGLNOvOvpCN6ypna";
+        //final String fileUrl = "https://drive.google.com/uc?export=download&id=1Ul-vu8XCKEUkCIrnAGLNOvOvpCN6ypna";
+        //assert test.equals(fileUrl);
         try {
             URI fileUri = new URI(fileUrl);
             URL url = fileUri.toURL();
